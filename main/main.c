7b448575a94625c30803891d5db92dfbf6f2cc68
root-kidik <rtkid.nik@gmail.com>
2025-03-06 06:11:19 +0300

    chore: simplif

--- main/main.c
+++ main/main.c
@@ -164,19 +164,10 @@ void wifi_init_sta(void)
 
 void app_main(void)
 {
-    //Initialize NVS
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
-    {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
+    ESP_ERROR_CHECK(nvs_flash_init());
 
     ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
     wifi_init_sta();
 
     while (!is_wifi_connected);
-
-    
 }
